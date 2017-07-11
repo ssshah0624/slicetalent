c724994f21d21ff468a4d518779989f45110446e
ssshah0624 <ssshah0624@gmail.com>
2017-07-11 15:17:05 -0700

    more logos

--- index.php
+++ index.php
@@ -2,9 +2,16 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <!-- Change the width and height values to suit you best -->
-<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:80vH;"></div>
+<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:calc(100vH-20px);"></div>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
 <div style="width:100%;height:20vH;">
-	<img src="http://www.ycombinator.com/images/ycombinator-logo-fb889e2e.png" alt="Y Combinator" style="100px;height:20vH;">
+	<table style="width:100%">
+	  <tr>
+	  	<th><img src="http://www.ycombinator.com/images/ycombinator-logo-fb889e2e.png" alt="Y Combinator" style="width:20px;height:20px;"></th>
+		<th><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Google-favicon-2015.png/150px-Google-favicon-2015.png" alt="Google" style="width:20px;height:20px;"></th> 
+		<th><img src="https://qph.ec.quoracdn.net/main-qimg-c73e4e4396325a9e636ad2b94b64601f" alt="Palantir" style="width:20px;height:20px;"></th>
+	  </tr>
+	</table>
+	
 </div>
 </html>
