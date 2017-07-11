9670a2b339c71b9f46a384c0ae8db87e5abcc917
ssshah0624 <ssshah0624@gmail.com>
2017-07-11 14:42:10 -0700

    adjusting height

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
   </style>
 </head>
 <body>
-  <iframe id="typeform-full" width="100%" height="100%" frameborder="0" allowtransparency="true" src="https://sunny269.typeform.com/to/mMMGSc"></iframe>
+  <iframe id="typeform-full" width="100%" height="80%" frameborder="0" allowtransparency="true" src="https://sunny269.typeform.com/to/mMMGSc"></iframe>
   <script type="text/javascript" src="https://s3-eu-west-1.amazonaws.com/share.typeform.com/embed.js"></script>
   <img src="http://www.ycombinator.com/images/ycombinator-logo-fb889e2e.png" alt="Y Combinator" style="width:304px;height:228px;">
 </body>
