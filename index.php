932b7ecba22dca7caf6bc7f0467085f9d9f1ea3e
ssshah0624 <ssshah0624@gmail.com>
2017-07-10 18:40:21 -0700

    ok

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+
+<!-- Change the width and height values to suit you best -->
+<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Phoenix Talent" style="width:100%;height:500px;"></div>
+<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+<div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;">Powered by<a href="https://www.typeform.com/examples/?utm_campaign=mMMGSc&amp;utm_source=typeform.com-9056113-Basic&amp;utm_medium=typeform&amp;utm_content=typeform-embedded-poweredbytypeform&amp;utm_term=EN" style="color: #999" target="_blank">Typeform</a></div>
+
+
+</html>
