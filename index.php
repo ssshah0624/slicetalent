7770eef3eb4f1269143878bc38d0ed0cfedca631
ssshah0624 <ssshah0624@gmail.com>
2017-07-11 14:52:56 -0700

    testing images

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <!-- Change the width and height values to suit you best -->
-<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:80%;"></div>
+<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:80vH;"></div>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
-
+<div style="width:100%;height:20vH;">
+	<img src="http://www.ycombinator.com/images/ycombinator-logo-fb889e2e.png" alt="Y Combinator" style="100px;height:20vH;">
+</div>
 </html>
