d085f6dc2547f1e152e84c43a185120d68139bd4
ssshah0624 <ssshah0624@gmail.com>
2017-07-11 17:08:29 -0700

    adjust

--- index.php
+++ index.php
@@ -2,23 +2,21 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <!-- Change the width and height values to suit you best -->
-<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:95vH;"></div>
+<div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:90vH;"></div>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
 <div style="width:100%;height:20vH;">
 	<table style="width:100%">
 	  <tr>
 	  	<th><img src="http://www.ycombinator.com/images/ycombinator-logo-fb889e2e.png" alt="Y Combinator" style="width:30px;height:30px;"></th>
 		<th><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Google-favicon-2015.png/150px-Google-favicon-2015.png" alt="Google" style="width:30px;height:30px;"></th> 
-		<th><img src="https://qph.ec.quoracdn.net/main-qimg-c73e4e4396325a9e636ad2b94b64601f" alt="Palantir" style="width:30px;height:30px;"></th>
-		<th><img src="https://d1qb2nb5cznatu.cloudfront.net/startups/i/3015975-0546c6936033276241410a958949f928-medium_jpg.jpg" alt="Alpha" style="width:30px;height:30px;"></th> 
 		<th><img src="http://www.turnerduckworth.com/media/filer_public/86/18/86187bcc-752a-46f4-94d8-0ce54b98cd46/td-amazon-smile-logo-01-large.jpg" alt="Amazon" style="width:30px;height:30px;"></th> 
 		<th><img src="https://cdn.worldvectorlogo.com/logos/slack-1.svg" alt="Slack" style="width:30px;height:30px;"></th> 
-		<th><img src="https://www.insightpartners.com/assets/public/img/logo.svg" alt="Insight Venture Partners" style="width:30px;height:30px;"></th> 
-		<th><img src="http://logok.org/wp-content/uploads/2014/10/EA-logo-black.png" alt="EA Games" style="width:30px;height:30px;"></th> 
 		<th><img src="https://static1.squarespace.com/static/5134cbefe4b0c6fb04df8065/t/587fa2f4725e255a02f090cb/1484759799314/squarespace-logo-symbol-black.jpg" alt="Squarespace" style="width:30px;height:30px;"></th> 
 		<th><img src="https://sendgrid.com/wp-content/themes/sgdotcom/pages/resource/brand/2016/SendGrid-Logomark.png" alt="Sendgrid" style="width:30px;height:30px;"></th> 
-	
+		<th><img src="https://upload.wikimedia.org/wikipedia/en/7/71/Optimizely-logo.png" alt="Optimizely" style="width:30px;height:30px;"></th> 
 	  </tr>
+	  <tr></tr>
+	  <tr></tr>
 	</table>
 	
 </div>
