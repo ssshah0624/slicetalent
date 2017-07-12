8ca3367a3d5955cd84d30bb606da6be1aee1721e
ssshah0624 <ssshah0624@gmail.com>
2017-07-11 17:12:51 -0700

    no scroll

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <!-- Change the width and height values to suit you best -->
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
+<style>
+	body, html {height:100%;}
+	body {overflow: hidden; }
+</style>
+
+<script>
+	window.addEventListener("keydown", function(e) {
+	    // space, page up, page down and arrow keys:
+	    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+	        e.preventDefault();
+	    }
+	}, false);
+</script>
+
+
 <!-- Change the width and height values to suit you best -->
 <div class="typeform-widget" data-url="https://sunny269.typeform.com/to/mMMGSc" data-text="Slice Talent" style="width:100%;height:90vH;"></div>
 <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
@@ -15,9 +30,6 @@
 		<th><img src="https://sendgrid.com/wp-content/themes/sgdotcom/pages/resource/brand/2016/SendGrid-Logomark.png" alt="Sendgrid" style="width:30px;height:30px;"></th> 
 		<th><img src="https://upload.wikimedia.org/wikipedia/en/7/71/Optimizely-logo.png" alt="Optimizely" style="width:30px;height:30px;"></th> 
 	  </tr>
-	  <tr></tr>
-	  <tr></tr>
 	</table>
-	
 </div>
 </html>
